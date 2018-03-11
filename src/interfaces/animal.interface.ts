50299b7dffb0c1c1f562ddbaf6014673bfb1ce2c
Samuel Polo <samuelpp1996@hotmail.com>
2018-03-11 13:10:13 -0500

    First Commit

---
+++ src/interfaces/animal.interface.ts
@@ -0,0 +1,7 @@
+export interface Animal {
+    nombre: string;
+    imagen: string;
+    audio: string;
+    duracion: number;
+    reproduciendo: boolean;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,89 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ANIMALES } from '../../data/data.animales';
+
+import { Animal } from '../../interfaces/animal.interface';
+
+import {Refresher, reorderArray} from 'ionic-angular';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando:boolean = false;
 
+  constructor() {
+
+    this.animales = ANIMALES.slice(0);
+
+  }
+
+  reproducir(animal: Animal) {
+
+    this.pausarAudio(animal);
+
+    if( animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+
+  }
+
+  private pausarAudio(animalSel: Animal) {
+
+    clearTimeout(this.audioTiempo);
+
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrarAnimal (idx: number) {
+    
+    this.animales.splice(idx,1);
+
+  }
+
+  recargarAnimales( refresher:Refresher) {
+    console.log("Inicio del refresh");
+
+    setTimeout( ()=> {
+
+        console.log("Terminó el refresh");
+        this.animales = ANIMALES.slice(0);
+
+        refresher.complete();
+
+    }, 1500)
+
+  }
+
+  reordenarAnimales(indices:any) {
+    console.log(indices);
+    this.animales = reorderArray(this.animales, indices);
   }
 
 }
